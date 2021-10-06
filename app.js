6e1d51dc7e60253cf4743ec60fedff6f24c6cba1
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-10-06 16:01:51 +0200

    refdv

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ var ctx = canvas.getContext("2d");
 
 function drawBall(bx, by) {
     ctx.beginPath();
-    ctx.arc(by, bx, 10, 0, Math.PI*2);
+    ctx.arc(bx, by, 10, 0, Math.PI*2);
     ctx.fillStyle = "#fff";
     ctx.fill();
     ctx.closePath();
-}
-  
-function draw(mx, my) {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall(mx, my);
 }
 
 canvas.addEventListener("mousemove", function(e){
