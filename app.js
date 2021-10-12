bd329d5aea68b8e49d5753895f635f3d766c9882
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-10-12 10:38:17 +0200

    zertgh

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ gameBox.on("mousemove", function (e) {
 
     console.log(coord);
 
-    // d3.select("circle")
-    //     .attr("transform", `translate(${pointer[0]}, ${pointer[1]})`);
+    // d3.select(".cls-1")
+    //     .attr("transform", `translate(-135,-185) scale(0.5) rotate(${coord}, 320, 320 )`);
 })
