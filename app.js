1042fa379fbfacd4dfd4a2d19390f175d923d617
gilyann <gilyann.padre@gmail.com>
2021-11-12 13:28:26 +0100

    changements

--- app.js
+++ app.js
@@ -6,102 +6,187 @@ let compteur = 0;
 let tabCoord = [];
 
 function entierAlea(n) {
-    return Math.floor(Math.random()*n);
+  return Math.floor(Math.random() * n);
 }
 
 gameBox.on("mousemove", function (e) {
+  let pointer = d3.pointer(e);
 
-    let pointer = d3.pointer(e);
+  mouseX = pointer[0];
+  mouseY = pointer[1];
 
-    mouseX = pointer[0];
-    mouseY = pointer[1];
+  coord = 90 - (Math.atan2(pointer[0], pointer[1]) * 180) / Math.PI;
 
-    coord = 90 - (Math.atan2(pointer[0], pointer[1]) * 180 / Math.PI);
-
-     d3.select(".jony")
-      .style("transform", `rotate(${coord}deg)`);
-})
+  d3.select(".jony").style("transform", `rotate(${coord}deg)`);
+});
 
 function updateDOM() {
-    let join=gameBox
-        .selectAll("path")
-        .data(tabCoord, d=> d.id);
+  let join = gameBox.selectAll("path").data(tabCoord, (d) => d.id);
 
-    join.enter()
-        .append("path")
-        .attr("d","M0,0 L-10,0 M0,0 L8.1,5.9 M0,0 L8.1,-5.9 M0,0 L-3.1,9.5 M0,0 L-3.1,-9.5")
-        .style("stroke", "red");
-    
-    join.exit()
-        .remove();
+  join
+    .enter()
+    .append("path")
+    .attr(
+      "d",
+      "M0,0 L-10,0 M0,0 L8.1,5.9 M0,0 L8.1,-5.9 M0,0 L-3.1,9.5 M0,0 L-3.1,-9.5"
+    )
+    .style("stroke", "red");
 
+  join.exit().remove();
 
-    updateTransforms();
+  updateTransforms();
 }
 
-function updateTransforms(){
-    gameBox
+function updateTransforms() {
+  gameBox
     .selectAll("path")
-    .attr("transform", d=>`translate(${d.x}, ${d.y})`);          
+    .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
 }
 
 // initialement: on ajoute les Ã©toiles
 updateDOM();
 
-function deplacePoint(c){
-    
-    if(mouseX >= 0 && mouseY >= 0){
-        c.x += 1 ;
-        c.y += 1 ;
-    }else if(mouseX <= 0 && mouseY >= 0){
-        c.x -= 1 ;
-        c.y += 1 ;
-    }else if(mouseX <= 0 && mouseY <= 0){
-        c.x -= 1 ;
-        c.y -= 1 ;
-    }else if(mouseX >= 0 && mouseY <= 0){
-        c.x += 1 ;
-        c.y -= 1 ;
-    }
+function deplacePoint(c) {
+  if (mouseX >= 0 && mouseY >= 0) {
+    c.x += 1;
+    c.y += 1;
+  } else if (mouseX <= 0 && mouseY >= 0) {
+    c.x -= 1;
+    c.y += 1;
+  } else if (mouseX <= 0 && mouseY <= 0) {
+    c.x -= 1;
+    c.y -= 1;
+  } else if (mouseX >= 0 && mouseY <= 0) {
+    c.x += 1;
+    c.y -= 1;
+  }
 }
 
-function pointVisible(c){ 
-    return c.x<250 && c.x>-250 &&  c.y<350 && c.y>-350;
+function pointVisible(c) {
+  return c.x < 250 && c.x > -250 && c.y < 350 && c.y > -350;
 }
 //toutes les 100ms, on ne modifie que les coordonnÃ©es
-setInterval(function(){
-    tabCoord.forEach(deplacePoint);
-    if (tabCoord.every(pointVisible)) { 
-        //tous les points dans coordonnÃ©es ont pointVisible(c) = true
-        updateTransforms();        
-    } else { 
-        //au moins un point cachÃ©, on le retire du tableau
-        tabCoord=tabCoord.filter(pointVisible);
-        updateDOM();
-    }
+setInterval(function () {
+  tabCoord.forEach(deplacePoint);
+  if (tabCoord.every(pointVisible)) {
+    //tous les points dans coordonnÃ©es ont pointVisible(c) = true
+    updateTransforms();
+  } else {
+    //au moins un point cachÃ©, on le retire du tableau
+    tabCoord = tabCoord.filter(pointVisible);
+    updateDOM();
+  }
 }, 10);
 
-
-
 //toutes les 1000ms, on insÃ¨re une Ã©toile
-setInterval(function(){    
-    compteur++;
-    tabCoord.push({x:0, y:0, id: compteur});
-    updateDOM();
+setInterval(function () {
+  compteur++;
+  tabCoord.push({ x: 0, y: 0, id: compteur });
+  updateDOM();
 }, 1000);
 
+function placeAmis(position) {
+  switch (position) {
+    case 0:
+      x = -350;
+      y = -250;
+      r = 135;
+      return [x,y,r]
+      
+    case 1:
+      x = 525;
+      y = -250;
+      r = 180;
+      return [x,y,r]
+      
+    case 2:
+      x = 700;
+      y = -250;
+      r = -135;
+      return [x,y,r]
+      
+    case 3:
+      x = 700;
+      y = -350;
+      r = -90;
+      return [x,y,r]
+      
+    case 4:
+      x = 700;
+      y = 500;
+      r = 45;
+      return [x,y,r]
+      
+    case 5:
+      x = 250;
+      y = 500;
+      r = 0;
+      return [x,y,r]
+      
+    case 6:
+      x = -350;
+      y = 500;
+      r = 45;
+      return [x,y,r]
+      
+    case 7:
+      x = -350;
+      y = 350;
+      r = 90;
+      return [x,y,r]
+      
+  }
+}
+
+let Amis = []
+
+setInterval(function () {
+  compteur++;
+  let position = placeAmis(entierAlea(8))
+  console.log(position,entierAlea(8))
+  Amis.push({ x: 0, y: 0, id: compteur });
+  updateDOM();
+}, 1000);
+
+function affichageEnemmi(){
+
+    let join = gameBox.selectAll("path").data(Amis, (d) => d.id);
+
+    join
+      .enter()
+      .append("path")
+      .attr(
+        "d",
+        "M0,0 L-10,0 M0,0 L8.1,5.9 M0,0 L8.1,-5.9 M0,0 L-3.1,9.5 M0,0 L-3.1,-9.5"
+      )
+      .attr("class","Amis")
+      .style("stroke", "black");
+  
+    join.exit().remove();
+  
+    updateAmis();
+  }
 
 
+  function updateAmis(){
 
+  
+  gameBox
+  .selectAll(".Amis")
+  .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
 
+}
 
+setInterval(function () {
+    Amis.forEach(Ami=>{
 
+console.log(Ami)
 
+    })
+        
+    updateAmis();
+  }, 10);
 
+    
 
-
-
-
-
-
-
+    
