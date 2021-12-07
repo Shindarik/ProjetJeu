22112cfa13da4c35dee5327dc385fa9d55dfe757
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-12-07 20:49:10 +0100

    fgh

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let mouseX, mouseY;
 let vieJoueur = 3;
 let score = 0;
 let previousScore = 0;
-let pause = false;
+let pause = true;
 let gameOver = false;
-let menu = false;
+let menu = true;
 
 
 gameBox.on("mousemove", function (e) {
@@ -29,11 +29,31 @@ gameBox.on("mousemove", function (e) {
 
 
 window.addEventListener("keypress", (e) =>{
-  if(e.key == 'p' && pause == false && gameOver == false){
+  if(e.key == 'p' && pause == false && gameOver == false && menu == false){
     pause = true;
     d3.select(".pauseScreen").style("display", "flex");
-  }else if(e.key == 'p' && pause == true && gameOver == false){
+  }else if(e.key == 'p' && pause == true && gameOver == false && menu == false){
     pause = false;
     d3.select(".pauseScreen").style("display", "none");
   }
+});
+
+document.querySelector(".skinReset").addEventListener("click", ()=>{
+
+  let invertTab = [0, 100];
+  let colorLight = ["yellow", "red", "white", "black", "blue", "green", "pink", "violet", "orange"];
+
+  let colorSkinHue = entierAlea(360);
+  let colorSkinInvert = invertTab[entierMinMax(0,1)];
+  let colorSkinLight = colorLight[entierMinMax(0,colorLight.length)];
+
+  console.log(colorSkinHue, colorSkinInvert);
+  d3.select(".skin").style("filter", `hue-rotate(${colorSkinHue}deg) invert(${colorSkinInvert}%) drop-shadow(0px 0px 3px ${colorSkinLight})`);
+  d3.select(".jony").style("filter", `hue-rotate(${colorSkinHue}deg) invert(${colorSkinInvert}%) drop-shadow(0px 0px 3px ${colorSkinLight})`);
+});
+
+document.querySelector(".start").addEventListener("click", ()=>{
+  menu=false;
+  pause=false;
+  d3.select(".menuScreen").style("display", "none");
 });
